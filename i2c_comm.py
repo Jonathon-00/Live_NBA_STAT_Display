f607c5742e373181dd66b23bf0dae3ac0296b19f
Jonathon-00 <jonathonelliott20@gmail.com>
2024-08-23 23:30:46 -0700

    Update i2c_comm.py

--- i2c_comm.py
+++ i2c_comm.py
@@ -1,46 +1,20 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-"""
-This demo will fill the screen with white, draw a black box on top
-and then print Hello World! in the center of the display
-
-This example is for use on (Linux) computers that are using CPython with
-Adafruit Blinka to support CircuitPython libraries. CircuitPython does
-not support PIL/pillow (python imaging library)!
-"""
-
 import board
-import digitalio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
-# Define the Reset Pin
-oled_reset = digitalio.DigitalInOut(board.D4)
 
-# Change these
-# to the right size for your display!
 WIDTH = 128
-HEIGHT = 64  # Change to 64 if needed
-BORDER = 5
+HEIGHT = 64 
+BORDER = 3
 
-# Use for I2C.
 i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
-
-# Use for SPI
-# spi = board.SPI()
-# oled_cs = digitalio.DigitalInOut(board.D5)
-# oled_dc = digitalio.DigitalInOut(board.D6)
-# oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, oled_dc, oled_reset, oled_cs)
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
 
 # Clear display.
 oled.fill(0)
 oled.show()
 
 # Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
 image = Image.new("1", (oled.width, oled.height))
 
 # Get drawing object to draw on image.
